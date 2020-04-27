b7cc0a862fa95a71e7c122da6f0c1425cb9c886e
gaohx <1109546392@qq.com>
2020-04-27 18:58:20 +0800

    init

--- src/test/java/com/jack/elasticsearch/ElasticsearchApplicationTests.java
+++ src/test/java/com/jack/elasticsearch/ElasticsearchApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ElasticsearchApplicationTests {
-
+//https://mp.weixin.qq.com/s?__biz=MzAxNjk4ODE4OQ==&mid=2247488586&idx=2&sn=7a5ed0ed084f3a9f20333e3616f20439&chksm=9bed3538ac9abc2e22aacb3e8c12adbf7334965473c99a562b67a1e5f05ba11f63ac53aeaa59&scene=126&sessionid=1586399008&key=a3ba4d6096b52af2b1e2029d632c0b2204ab06535f23e8c1d01b4e9082653f3e38667d905c477589d6fa80a4d604ee90c00cbcacb3cbc015e5badba40e132bac362c2a2c9e8afcd1cdd17ec26f93c06c&ascene=1&uin=Nzg0MDA0NzYx&devicetype=Windows+7&version=62080079&lang=zh_CN&exportkey=AYpyKuos6b7NMLM0IFBpdqU%3D&pass_ticket=1VgJex%2BSUCxi%2B8%2Feg%2FkWbogo7rlGSi77odyewxon%2Fpe5KuiK6eeDKAcG%2FcLnOh1C
     @Test
     void contextLoads() {
     }
