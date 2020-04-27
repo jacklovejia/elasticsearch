b7b80fb88e7d44866f2e73d4e9025169991f84ef
gaohx <1109546392@qq.com>
2020-04-27 18:56:23 +0800

    init

---
+++ src/test/java/com/jack/elasticsearch/Test.java
@@ -0,0 +1,18 @@
+package com.jack.elasticsearch;
+
+import java.lang.annotation.Native;
+
+public class Test {
+    public static void main(String[] args) {
+          final int   MIN_VALUE = 0x80000000;
+
+        /**
+         * A constant holding the maximum value an {@code int} can
+         * have, 2<sup>31</sup>-1.
+         */
+         final int   MAX_VALUE = 0x7fffffff;
+
+        System.out.println(MIN_VALUE);
+        System.out.println(MAX_VALUE);
+    }
+}
