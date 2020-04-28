d2c17ead7869fcd0fb3a3e983367200e95d01ab4
jack <1109546392@qq.com>
2020-04-28 21:25:27 +0800

    测试jenkins

---
+++ src/main/java/com/jack/elasticsearch/controller/Test.java
@@ -0,0 +1,16 @@
+package com.jack.elasticsearch.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Test {
+
+    @RequestMapping("/name/{name}")
+    public String test(@PathVariable String name){
+        return "你好"+name;
+    }
+
+}
