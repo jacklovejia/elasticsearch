194681bfdcf2bab1cde4a8655bba93444a0cce62
gaohx <1109546392@qq.com>
2020-05-01 17:57:04 +0800

    测试

--- src/main/java/com/jack/elasticsearch/controller/Test.java
+++ src/main/java/com/jack/elasticsearch/controller/Test.java
@@ -1,16 +1,51 @@
 package com.jack.elasticsearch.controller;
 
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
 @RestController
 @RequestMapping("/test")
-public class Test {
+public class Test implements BeanFactoryAware , BeanNameAware, BeanPostProcessor {
+
+    @PostConstruct
+    public void init(){
+        System.out.println("初始化");
+    }
+    @PreDestroy
+    public void destroy(){
+        System.out.println("销毁");
+    }
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+        return null;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+        return null;
+    }
 
     @RequestMapping("/name/{name}")
     public String test(@PathVariable String name){
         return "你好"+name+",么么哒";
     }
 
+    @Override
+    public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+        System.out.println("factory:" + beanFactory.getBean("test"));
+    }
+
+    @Override
+    public void setBeanName(String s) {
+        System.out.println("name:"+s);
+    }
 }
