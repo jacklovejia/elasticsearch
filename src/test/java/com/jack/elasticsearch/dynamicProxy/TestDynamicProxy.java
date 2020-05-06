8ac9498659c2de41f1e577027714878731dae414
gaohx <1109546392@qq.com>
2020-05-06 10:11:07 +0800

    动态代理

--- src/test/java/com/jack/elasticsearch/dynamicProxy/TestDynamicProxy.java
+++ src/test/java/com/jack/elasticsearch/dynamicProxy/TestDynamicProxy.java
@@ -1,5 +1,6 @@
 package com.jack.elasticsearch.dynamicProxy;
 
+import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
 
 public class TestDynamicProxy {
@@ -11,10 +12,20 @@ public class TestDynamicProxy {
          *         main 测试
          */
         DynamicProxy d = new DynamicProxy(new CalcServiceImpl());
-
-        CalcService c = (CalcService) Proxy.newProxyInstance(CalcService.class.getClassLoader(), new Class[]{CalcService.class}, d);
+        CalcService c = (CalcService) Proxy.newProxyInstance(CalcService.class.getClassLoader(),
+                new Class[]{CalcService.class},
+                d);
         Integer add = c.add(1, 1);
         System.out.println(add);
+// ===============================================================================================================================
+        CalcServiceImpl calcService = new CalcServiceImpl();
+        DynamicProxy dynamicProxy = new DynamicProxy(calcService);
+
+        CalcService o = (CalcService) Proxy.newProxyInstance(dynamicProxy.getClass().getClassLoader(), // 代理类的类加载器
+                calcService.getClass().getInterfaces(), // 目标接口
+                dynamicProxy); // 代理类实例
+        System.out.println(o.add(1, 2));
+
 
     }
 }
