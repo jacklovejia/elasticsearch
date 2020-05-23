594cbb850887ab08c301d6c239bd08db3d46929b
jack <1109546392@qq.com>
2020-05-23 18:12:26 +0800

    所有线程全部执行过后,打印ok

--- src/test/java/com/jack/elasticsearch/testThread/Test.java
+++ src/test/java/com/jack/elasticsearch/testThread/Test.java
@@ -1,10 +1,16 @@
 package com.jack.elasticsearch.testThread;
 
+import java.util.concurrent.CountDownLatch;
+
 public class Test {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+
+        
+        CountDownLatch countDownLatch = new CountDownLatch(2);
         Thread thread1 = new Thread(() -> {
             try {
-                Thread.sleep(1000);
+                Thread.sleep(10000);
+                countDownLatch.countDown();
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
@@ -17,11 +23,13 @@ public class Test {
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
+            countDownLatch.countDown();
             System.out.println("2");
         }, "2");
 
         thread2.start();
         thread1.start();
-
+        countDownLatch.await();
+        System.out.println("ok");
     }
 }
